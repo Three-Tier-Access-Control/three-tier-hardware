d1212ceced98d4d08d27331f9661ab9928b562d8
Ashley Shumba <ashleytshumba@gmail.com>
2022-04-01 15:19:25 +0200

    turn on and turn off

--- app/routers/led.py
+++ app/routers/led.py
@@ -2,23 +2,26 @@ from fastapi import APIRouter
 from gpiozero import LED
 from time import sleep
 from schema import Pin
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+
+
 
 router = APIRouter(tags=["LED"])
 
 
 @router.post("/turn-on-led")
 async def turn_on_led(pin: Pin):
-    led = LED(pin.number)
-    led.on()
-    sleep(1)
+    GPIO.output(pin.number, GPIO.HIGH)
     return {"data": "Turned on LED"}
 
 
 @router.post("/turn-off-led")
 async def turn_off_led(pin: Pin):
-    led = LED(pin.number)
-    led.off()
-    sleep(1)
+    GPIO.output(pin.number, GPIO.HIGH)
     return {"data": "Turned off LED"}
 
 @router.post("/blink-led")
