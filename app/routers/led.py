4c9fda5e5d9d1e5f12d65593325af6df1da838f4
Ashley Shumba <ashleytshumba@gmail.com>
2022-04-01 22:38:48 +0200

    dynamic blinking

--- app/routers/led.py
+++ app/routers/led.py
@@ -6,8 +6,6 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17, GPIO.OUT)
-
 
 
 router = APIRouter(tags=["LED"])
@@ -15,17 +13,20 @@ router = APIRouter(tags=["LED"])
 
 @router.post("/turn-on-led")
 async def turn_on_led(pin: Pin):
+    GPIO.setup(pin.number, GPIO.OUT)
     GPIO.output(pin.number, GPIO.HIGH)
     return {"data": "Turned on LED"}
 
 
 @router.post("/turn-off-led")
 async def turn_off_led(pin: Pin):
+    GPIO.setup(pin.number, GPIO.OUT)
     GPIO.output(pin.number, GPIO.LOW)
     return {"data": "Turned off LED"}
 
 @router.post("/blink-led")
 async def blink_led(pin: Pin):
+    GPIO.setup(pin.number, GPIO.OUT)
     GPIO.output(pin.number, GPIO.HIGH)
     sleep(1)
     GPIO.output(pin.number, GPIO.LOW)
