c1ef56f4cce7afca4eaffba3c6b8e1784aa67080
Shumba Ashley <ashleytshumba@gmail.com>
2022-05-25 18:27:36 +0200

    gpio setup

--- app/routers/gpio.py
+++ app/routers/gpio.py
@@ -14,6 +14,8 @@ router = APIRouter(tags=["GPIO Pins"])
 @router.post("/turn-on")
 async def turn_on_gpio_pin(pin: Pin):
     number = pin.number
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     GPIO.setup(number, GPIO.OUT)
     GPIO.output(number, GPIO.HIGH)
     return {"data": f"Pin {number} has been turned on"}
@@ -21,6 +23,8 @@ async def turn_on_gpio_pin(pin: Pin):
 
 @router.post("/turn-off")
 async def turn_off_gpio_pin(pin: Pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
     number = pin.number
     GPIO.setup(number, GPIO.OUT)
     GPIO.output(number, GPIO.LOW)
