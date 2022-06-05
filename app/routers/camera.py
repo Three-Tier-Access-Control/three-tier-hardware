982bb3879b4a9db305c4724111194128848c21f8
Shumba Ashley <ashleytshumba@gmail.com>
2022-06-05 23:50:44 +0200

    take picture

--- app/routers/camera.py
+++ app/routers/camera.py
@@ -8,23 +8,13 @@ def take_picture():
     cam = cv2.VideoCapture(2)
 
     while True:
-        result, image = cam.read()
-        if result:
-            cv2.imshow("Three Tier Security", image)
+        ret, image = cam.read()
+        cv2.imshow('Image Test',image)
 
-            # saving image in local storage
+        if cv2.waitKey(1) & 0xFF == ord('c'):
             cv2.imwrite('/home/ashley/projects/project-samples/images/testimage.jpg', image)
+            break
 
-            # If keyboard interrupt occurs, destroy image
-            # window
-            cv2.waitKey(0)
-            cam.release()
-            cv2.destroyWindow("Three Tier Security")
-    
-        # If captured image is corrupted, moving to else part
-        else:
-            print("No image detected. Please! try again")
-            return {"detail": "Error: No image detected. Please! try again"}
-        
-        return {"data": "picture taken"}
-
+    cam.release()
+    cv2.destroyAllWindows()
+    return {"data": "picture taken"}
