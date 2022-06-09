ebc64c21a8c574f6acc5ef887e4e5ea62903b2f6
Shumba Ashley <ashleytshumba@gmail.com>
2022-06-09 13:27:08 +0200

    tag read

--- app/routers/rfid.py
+++ app/routers/rfid.py
@@ -18,7 +18,7 @@ async def read_rfid_card():
         print("Place your tag to read...")
         print_to_lcd("Place card")
         uid, employee_id = reader.read()
-        print(f"Tag read #: {id} \n Data: {employee_id}")
+        print(f"Tag read #: {uid} \n Data: {employee_id}")
         print_to_lcd("Card read!")
         return {"data": {"uid": uid, "employee_id": employee_id}}
     except Exception as e:
